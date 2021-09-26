fbc03441e62f5d6a509695ec5fa096ac349fa617
Farhan <farhanbajwa46@gmail.com>
2021-09-26 07:21:54 +0500

    tea time

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("helloo worlds");
+  const linkElement = screen.getByText("hello worlds");
   expect(linkElement).toBeInTheDocument();
 });
